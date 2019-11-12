69b44850e22ee1b0b08db686ff34a576200d4841
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-12 17:37:04 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,22 +5,25 @@ def directors_totals(nds)
   
 row_index = 0
 
-while row_index < nds.count do
- directors_name = nds[row_index][:name]
+while row_index < nds.length do
+directors_name = nds[row_index][:name]
   # The directors_name is a variable of all the directors in the nds.
   
-  movies = nds[row_index][:movies].sum
+ movies = nds[row_index][:movies]
   # The movies variable show the names of all the movies.
  
-    # movies[row_index][:worldwide_gross]
-# directors_gross = movies[row_index][:worldwide_gross]
-# The directors_gross is a variable of the sum of all of each directors movies.
+directors_info = directors_name, movies
+# directors_info is a variable containing all information of the first 2 variables.
+# gross = movies[row_index][:worldwide_gross]
+## directors_gross = movies[column_index][:worldwide_gross]
+# # The directors_gross is a variable of the sum of all of each directors movies.
 
 # totals = {directors_name => directors_gross}
 # totals is a hash combining the two varaibles.
 
   row_index += 1
-pp movies
+  # pp directors_name, movies
+pp directors_info[:worldwide_gross]
 end
 
   # Remember, it's always OK to pretty print what you get *in* to make sure
