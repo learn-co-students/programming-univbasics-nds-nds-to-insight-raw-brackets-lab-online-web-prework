e4707651307fc93981e3378b9bb67355aa7ba49b
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-13 16:40:48 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,47 +4,24 @@ require 'directors_database'
 def directors_totals(nds)
   
 row_index = 0
-
+gross = {}
 while row_index < nds.length do
 directors_name = nds[row_index][:name]
   # The directors_name is a variable of all the directors in the nds.
-  
+gross[directors_name] = 0 
+# This takes the variable directors_name, adds it into the gross hash and gives it a value of 0 as a counter.
  movies = nds[row_index][:movies]
   # The movies variable show the names of all the movies.
+ element_index = 0 
  
-directors_info = directors_name, movies
- # directors_info is a variable containing all information of the first 2 variables.
-
-## directors_gross = movies[column_index][:worldwide_gross]
-# # The directors_gross is a variable of the sum of all of each directors movies.
-
-# totals = {directors_name => directors_gross}
-# totals is a hash combining the two varaibles.
-
+ while element_index < movies.length
+ gross[directors_name] += movies[element_index][:worldwide_gross]
+ 
+ element_index += 1 
+ end
+ 
   row_index += 1
-  pp nds[row_index]
-  # pp directors_info[0...7]
-  # pp directors_name, movies[0..7][1][:worldwide_gross]
-  # pp directors_name, movies
-# pp directors_info
 end
 
-
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  
+pp gross
 end
