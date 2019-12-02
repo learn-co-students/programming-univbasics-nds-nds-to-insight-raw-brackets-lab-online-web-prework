2efedf6cfbb408c930ecdee6e5c0ed7c4e274594
thomas dubiel <td1729@hotmail.com>
2019-12-02 17:38:57 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Construct Insights from an NDS With the `[]`s
+le# Construct Insights from an NDS With the `[]`s
 
 ## Introduction
 
--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,6 +7,7 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  pp nds
   result = {
   }
   #
@@ -18,5 +19,22 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  
+  total_gross = 0
+  name_index = 0 
+  puts nds.size
+  puts nds[name_index][:movies][1][:worldwide_gross]
+  while name_index < nds.size do 
+    movies_index = 0
+    director_total_gross = 0
+    while movies_index < nds[name_index][:movies].size do
+      director_total_gross += nds[name_index][:movies][movies_index][:worldwide_gross] 
+      movies_index += 1
+    end 
+    result["#{nds[name_index][:name]}"] = director_total_gross
+    name_index += 1
+  end
+  
+  result
+  
 end
