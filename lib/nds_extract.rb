734fe4ec6cce842ecb977d71babc6bedf79ef429
Khalid Tissire <ghettoguy7@gmail.com>
2019-12-26 21:04:48 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,20 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  result = {}
+  i = 0
+  len1 = directors_database.length
+  while i < len1 do
+    sum = 0
+    j = 0
+    len2 = directors_database[i][:movies].length 
+    while j < len2 do
+      sum += directors_database[i][:movies][j][:worldwide_gross]
+      result[directors_database[i][:name]] = sum
+      j += 1
+    end
+    i += 1
+  end
+  return result
+  #nil
 end
