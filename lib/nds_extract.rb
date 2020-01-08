0c6ebd12d9ce121b00da5a9580af8747ea5175dd
fadimata tillman <fadi.tillman@gmail.com>
2020-01-08 16:31:51 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,7 +1,22 @@
+pp require
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database'
+  require 'directors_database'
+
 
 def directors_totals(nds)
+       
+  directors_totals = {
+  "jean Pierre"=> [1234567890],
+  "Alice au  pays"=> [1234577890],
+  "Lapin fou "=>[1234709136], 
+  
+  
+}
+p directors_database
+  # I pretty print directors file 
+  # I created hash
+  #print to see what i 'm working with'
+  
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
@@ -16,7 +31,25 @@ def directors_totals(nds)
   # ...
   # ...
   #
-  #
+  grand_total = 0
+row_index = 0
+while row_index < directors_totals.length do
+  column_index = 0
+  while column_index < directors_totals[row_index].length do
+    inner_len = directors_totals[row_index][column_index].length
+    inner_index = 0
+    while inner_index < inner_len do
+    #total directors 
+   
+    grand_total += directors_totals[row_index][column_index][inner_index][:price]
+      inner_index += 1
+    end
+    column_index += 1
+  end
+  row_index += 1
+end
+ 
+p grand_total  
   # Be sure to return the result at the end!
   nil
 end
