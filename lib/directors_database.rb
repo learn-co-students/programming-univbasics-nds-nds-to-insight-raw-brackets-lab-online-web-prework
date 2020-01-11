a2402e4f2522367fd4547ca719f431e0fc501110
Timur Mishiev <timurmishiev@gmail.com>
2020-01-11 01:32:31 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -4,3 +4,37 @@ def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require_relative './directors_database'
+
+def directors_totals(source)
+  result = {}
+  director_index = 0
+  while director_index < source.size do
+    director = source[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
+end
+
+def gross_for_director(d)
+  total = 0
+  index = 0
+
+  while index < d[:movies].length do
+    total += d[:movies][index][:worldwide_gross]
+    index += 1
+  end
+
+  total
+end
+
+def list_of_directors(source)
+  
+end
+
+def total_gross(source)
+  
+end
