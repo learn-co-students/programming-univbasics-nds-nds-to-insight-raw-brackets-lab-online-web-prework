aed310e65af4121699e0c5817b84563d911bd80b
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 18:51:48 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,14 +2,38 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
+director_array = Array.new
+total_array = Array.new
+new_aoh = {}
+count = 0
+directors = nds.to_a.size
+
+
+  nds.each { |n| 
+    director_array << n[:name].to_s
   }
-  #
+  
+directors.times {
+  nds[count][:movies].each { |n| 
+      total_array << n[:worldwide_gross] 
+      new_aoh[director_array[count]] = total_array.sum.to_i
+  }
+  total_array.clear
+  count += 1
+}
+
+#puts director_array
+#puts total_array
+p new_aoh
+# puts directors
+end
+
+
+#p total 
+
+directors_totals(directors_database)
+
+#
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
   # ...
@@ -18,5 +42,34 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
-end
+  
+   # Remember, it's always OK to pretty print what you get *in* to make sure
+  # that you know what you're starting with!
+  #
+  #
+  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  
+# $LOAD_PATH.unshift(File.dirname(__FILE__))
+# require 'directors_database'
+
+# def directors_totals(nds)
+#   director_index = 0
+#   totals = {}
+
+#   while director_index < nds.length do
+#     director_name = nds[director_index][:name]
+#     totals[director_name] = 0
+#     movie_index = 0
+
+#     while movie_index < nds[director_index][:movies].length do
+#       totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+#       movie_index += 1
+#     end
+
+#     director_index += 1
+#   end
+
+#   p totals
+# end
+
+# directors_totals(directors_database)
