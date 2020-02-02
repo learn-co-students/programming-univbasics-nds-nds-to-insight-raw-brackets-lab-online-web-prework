4764f8af3ef4485bff41b1ea3d30f98ad4865957
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 18:53:23 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,17 +22,13 @@ directors.times {
   count += 1
 }
 
-#puts director_array
-#puts total_array
 p new_aoh
-# puts directors
 end
-
-
-#p total 
 
 directors_totals(directors_database)
 
+
+#========================================================================================
 #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
@@ -49,27 +45,3 @@ directors_totals(directors_database)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   
-# $LOAD_PATH.unshift(File.dirname(__FILE__))
-# require 'directors_database'
-
-# def directors_totals(nds)
-#   director_index = 0
-#   totals = {}
-
-#   while director_index < nds.length do
-#     director_name = nds[director_index][:name]
-#     totals[director_name] = 0
-#     movie_index = 0
-
-#     while movie_index < nds[director_index][:movies].length do
-#       totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
-#       movie_index += 1
-#     end
-
-#     director_index += 1
-#   end
-
-#   p totals
-# end
-
-# directors_totals(directors_database)
